1eb79f3484d9674ca4ed3e6fb4f6b81ab056f2a7
Amirah Yusof <mirez.data@gmail.com>
2025-07-04 21:41:59 +0800

    remove margin and padding in global css and apply desire layout in main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,15 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold mb-10">Welcome to Next.js Boilerplate 🚀</h1>
-      <div className="max-w-md text-center my-10">
+    <main className="flex flex-col min-h-screen items-center justify-center space-y-6">
+      <h1 className="text-3xl font-bold">Welcome to Next.js Boilerplate 🚀</h1>
+      <div className="max-w-md text-center rounded-lg shadow-lg">
         <p className="text-md">This is a simple starter template for Next.js applications.</p>
         <p className="text-md">It's consist Next.js, Tailwind CSS and Shadcn UI.</p>
         <p className="text-md">Feel free to customize it as per your requirements.</p>
       </div>
-      <div className="">
-        <Button className="mt-20 bg-blue-400 hover:bg-black/50">Let's create something amazing!</Button>
+      <div>
+        <Button className="p-5 bg-blue-400 hover:bg-black/50">Let's create something amazing!</Button>
       </div>
     </main>
   );
